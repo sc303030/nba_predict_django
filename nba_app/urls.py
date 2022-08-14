bfec4d880faf98deb9746bcd6dc6e80114b113ff
sc303030 <lkh6035@naver.com>
2022-08-14 19:55:22 +0900

    22.08.14 | add_viewset and router

--- nba_app/urls.py
+++ nba_app/urls.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers, serializers, viewsets
+from .views import PlayerViewSet
+
+router = routers.DefaultRouter()
+router.register(r'players', PlayerViewSet, basename='players')
 
 urlpatterns = [
+    path('', include(router.urls))
 ]
--- nba_app/views.py
+++ nba_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Player
+from .serializers import PlayerSerializers
+
 
 # Create your views here.
+class PlayerViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializers
