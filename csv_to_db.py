69020a402fca208afccdacc87ea89d7000366af6
sc303030 <lkh6035@naver.com>
2022-08-29 23:04:29 +0900

    22.08.29 | injury_into_db

--- csv_to_db.py
+++ csv_to_db.py
@@ -4,7 +4,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'nba_predict_django.settings'
 import django
 
 django.setup()
-from nba_app.models import Player
+from nba_app.models import Player, Injury
 
 from dataclasses import dataclass
 import pandas as pd
@@ -12,12 +12,21 @@ import pandas as pd
 
 @dataclass
 class PlayerIntoDb:
-    df: pd.DataFrame
+    player_df: pd.DataFrame
+    merged_df: pd.DataFrame
 
     def csv_to_list(self):
-        return self.df['name'].values, self.df['age'].values, self.df['year'].values
+        return self.player_df['name'].values, self.player_df['age'].values, self.player_df['year'].values
 
-    def into_db(self):
+    def merge_position(self):
+        _notes_drop = self.player_df.drop(['Notes'], axis=1, inplace=True)
+        df_injury_player_merge = pd.merge(_notes_drop, self.merged_df, left_on="Relinquished", right_on="name",
+                                          how='right')
+
+    def injury_info_db(self):
+        pass
+
+    def player_into_db(self):
         name_list, age_list, year_list = self.csv_to_list()
         players = []
         for name, age, year in zip(name_list, age_list, year_list):
@@ -26,11 +35,9 @@ class PlayerIntoDb:
 
 
 player_df = pd.read_csv('crawling/player_info.csv')
-injury_1998 = pd.read_csv("crawling/player_info.csv")
-merged_position = pd.read_csv("crawling/merged_position")
-injury_1998.drop(['Notes'], axis=1, inplace=True)
-df_injury_player_merge = pd.merge(injury_1998, merged_position, left_on="Relinquished", right_on="name", how='right')
+merged_df = pd.read_csv("crawling/nba_injury_merge_position.csv")
+
 print('Player 시작')
-player_info_db = PlayerIntoDb(player_df)
-player_info_db.into_db()
+player_info_db = PlayerIntoDb(player_df=player_df, merged_df=merged_df)
+player_info_db.player_into_db()
 print('Player 종료')
