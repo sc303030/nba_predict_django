b9556447543bde4cd54c8b37186bd28c59b83b2e
sc303030 <lkh6035@naver.com>
2022-09-13 21:58:12 +0900

    22.09.13 | add player_image_download

--- crawling/position_crawling.py
+++ crawling/position_crawling.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 import time
+import requests
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -16,13 +17,14 @@ def get_injury_name_dict():
 
     position_dic = {}
     for name in position_name:
-        position_dic[name] = 0
+        position_dic[name] = {}
     return nba_injury_merge, position_dic
 
 
 def crawling_position(position_dic):
     driver.get('https://www.nba.com/players')
     driver.find_elements(By.CSS_SELECTOR, '.Toggle_slider__hCMQQ')[0].click()
+    time.sleep(1)
     key_words = driver.find_elements(By.CSS_SELECTOR, '.Input_input__3YQfM')[-1]
     for names in list(position_dic.keys()):
         key_words.send_keys(names)
@@ -30,10 +32,24 @@ def crawling_position(position_dic):
         position_table = driver.find_elements(By.CSS_SELECTOR, '.players-list')[0]
         position_tr = position_table.find_elements(By.TAG_NAME, 'tr')[1]
         player_image_td = position_tr.find_elements(By.TAG_NAME, 'td')[0]
+        time.sleep(2)
         player_image_url = player_image_td.find_elements(By.TAG_NAME, 'img')
-        print(player_image_url[0].get_attribute('src'))
+        time.sleep(2)
+        player_image = player_image_url[0].get_attribute('src')
+        player_image = player_image.replace("260x190", "1040x760")
+        print(player_image)
+        headers = {
+            'User-Agent': 'Mozilla/5.0'
+        }
+        with requests.get(player_image,headers=headers) as u:
+            with open(f'C:\\Users\\gh\\nba_predict_django\\crawling\\img\\{names}.png', 'wb') as outfile:
+                time.sleep(1)
+                img = u.content
+                time.sleep(1)
+                outfile.write(img)
         position_td = position_tr.find_elements(By.TAG_NAME, 'td')[3].text
-        position_dic[names] = position_td
+        position_dic[names]['position'] = position_td
+        # position_dic[names]['img_url'] = player_image
         key_words.clear()
         time.sleep(1)
     return position_dic
@@ -46,7 +62,7 @@ def posi(x, position_dic):
 
 
 def write_to_dir(nba_injury_merge, position_dic):
-    nba_injury_merge['position'] = nba_injury_merge['name'].apply(lambda x: posi(x, position_dic))
+    nba_injury_merge[['position', 'img_url']] = nba_injury_merge['name'].apply(lambda x: posi(x, position_dic))
     nba_injury_merge.to_csv('crawling/nba_injury_merge_position.csv', mode='w', index=False)
 
 
