ee8e6ffb81c244faa28527146af01da59cfe5953
sc303030 <lkh6035@naver.com>
2022-09-16 22:59:42 +0900

    22.09.16 | img_update and nba_site class name change

--- crawling/position_crawling.py
+++ crawling/position_crawling.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 import time
 import requests
-
+from selenium.webdriver.common.keys import Keys
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 service = ChromeService(executable_path='crawling/chromedriver.exe')
@@ -23,9 +23,9 @@ def get_injury_name_dict():
 
 def crawling_position(position_dic):
     driver.get('https://www.nba.com/players')
-    driver.find_elements(By.CSS_SELECTOR, '.Toggle_slider__hCMQQ')[0].click()
+    driver.find_elements(By.CSS_SELECTOR, '.Toggle_slider__ln3dZ')[1].click()
     time.sleep(1)
-    key_words = driver.find_elements(By.CSS_SELECTOR, '.Input_input__3YQfM')[-1]
+    key_words = driver.find_elements(By.CSS_SELECTOR, '.Input_input__7s5ug')[-1]
     for names in list(position_dic.keys()):
         key_words.send_keys(names)
         time.sleep(1)
@@ -37,7 +37,6 @@ def crawling_position(position_dic):
         time.sleep(2)
         player_image = player_image_url[0].get_attribute('src')
         player_image = player_image.replace("260x190", "1040x760")
-        print(player_image)
         headers = {
             'User-Agent': 'Mozilla/5.0'
         }
@@ -48,8 +47,7 @@ def crawling_position(position_dic):
                 time.sleep(1)
                 outfile.write(img)
         position_td = position_tr.find_elements(By.TAG_NAME, 'td')[3].text
-        position_dic[names]['position'] = position_td
-        # position_dic[names]['img_url'] = player_image
+        position_dic[names] = position_td
         key_words.clear()
         time.sleep(1)
     return position_dic
@@ -62,7 +60,7 @@ def posi(x, position_dic):
 
 
 def write_to_dir(nba_injury_merge, position_dic):
-    nba_injury_merge[['position', 'img_url']] = nba_injury_merge['name'].apply(lambda x: posi(x, position_dic))
+    nba_injury_merge['position'] = nba_injury_merge['name'].apply(lambda x: posi(x, position_dic))
     nba_injury_merge.to_csv('crawling/nba_injury_merge_position.csv', mode='w', index=False)
 
 
