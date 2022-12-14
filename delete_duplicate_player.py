dfcdb095f63bc2a8c2972134fb48071f5ae383d0
sc303030 <lkh6035@naver.com>
2022-12-14 22:50:11 +0900

    22.12.14 | 현역 player 삭제

--- delete_duplicate_player.py
+++ delete_duplicate_player.py
@@ -7,10 +7,12 @@ django.setup()
 from nba_app.models import Player
 from django.db.models import Count
 
-delete_pk = []
 player_obj = Player.objects.values('name').annotate(cnt=Count('id')).filter(cnt__gte=2)
 for i in player_obj:
     name = i['name']
     obj = Player.objects.filter(name=name).order_by("retire_year")[0]
     obj.delete()
 
+activate_player = ["Ray Allen"]
+activate_player_obj = Player.objects.filter(name__in=activate_player)
+activate_player_obj.delete()
